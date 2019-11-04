8876464c7f3d888aef2cde4d7a032f043d4df1c9
Farhan Meah <fmeah2@gmail.com>
2019-11-03 19:33:13 -0600

    updated

---
+++ javascript/app.js
@@ -0,0 +1,19 @@
+// function merge(first, second){
+//     var a = first + ' love ' + second;
+//     console.log(a);
+// }
+
+// merge('I', 'you')
+
+// merge('I', 'coding')
+
+function merge(first, second){
+    var a = first + ' love ' + second;
+    return a;
+}
+
+var result1 = merge('I', 'you');
+var result2 = merge('I', 'coding');
+var result3 = merge('I', 'wife');
+
+console.log(result1, result2, result3);
