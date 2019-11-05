2a485fbc4506f85110b7eaf5010bae57b6908528
Farhan Meah <fmeah2@gmail.com>
2019-11-04 20:44:43 -0600

    Career

--- javascript/app.js
+++ javascript/app.js
@@ -7,13 +7,37 @@
 
 // merge('I', 'coding')
 
-function merge(first, second){
-    var a = first + ' love ' + second;
-    return a;
+// function merge(first, second){
+//     var a = first + ' love ' + second;
+//     return a;
+// }
+
+// var result1 = merge('I', 'you');
+// var result2 = merge('I', 'coding');
+// var result3 = merge('I', 'wife');
+
+// console.log(result1, result2, result3);
+
+// var name = prompt('What is your name?');
+// alert('Hi ' + name);
+
+// var dob = prompt('What year you were born?');
+// var ageTil = prompt('What is the current year?');
+// alert('Hi, you are ' + (ageTil - dob) + ' years od.');
+
+
+// function declaration / function statement
+function mergeText(text1, text2){
+    var merged = text1 + ' ' + text2;
+    console.log(merged)
 }
 
-var result1 = merge('I', 'you');
-var result2 = merge('I', 'coding');
-var result3 = merge('I', 'wife');
+// function expression
+var mergeTextTwo = function(text1, text2){
+    var merged= (text1 + ' ' + text2);
+    console.log(merged)
+}
 
-console.log(result1, result2, result3);
+// function invocation
+  mergeText('Farhan', 'Mia');
+  mergeTextTwo('Mike', 'Mia');
